a39bcaa35b89b1dc217108bd36588b0f591588e1
Justin Cullen <jcullen@mit.edu>
2012-02-27 14:40:30 -0500

    Vimrc and xmonad changes

--- xmonad.linkme/xmonad.hs
+++ xmonad.linkme/xmonad.hs
@@ -11,10 +11,26 @@ import System.IO
     -- If xmobar is in your $PATH, and its config is in ~/.xmobarrc you don't
     -- need the xmobar path or config file, use: xmproc <- spawnPipe "xmobar"
  
+myLayout = tiled ||| Mirror tiled ||| Full  
+ where  
+      -- default tiling algorithm partitions the screen into two panes  
+      tiled = Tall nmaster delta ratio  
+   
+      -- The default number of windows in the master pane  
+      nmaster = 1  
+   
+      -- Default proportion of screen occupied by master pane  
+      ratio = 2/3  
+   
+      -- Percent of screen to increment by when resizing panes  
+      delta = 5/100 
+
 -- Define terminal
 myTerminal = "sakura"
+
 -- Define amount and names of workspaces
 myWorkspaces = ["1:main","2:dev","3:www","4:media","5:mail","6:chat"]
+
 -- Define the workspace each application goes to
 myManageHook = composeAll
    [className =? "Firefox" --> doShift "3:www",
@@ -28,7 +44,10 @@ main = do
     xmproc <- spawnPipe "xmobar /home/jcullen/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = avoidStruts myLayout
+        , borderWidth = 2
+        , normalBorderColor = "#E6E6E6"
+        , focusedBorderColor = "#BF00FF"
         , startupHook = startup
         , terminal = myTerminal
         , workspaces = myWorkspaces
