721109dd1730790dfa22ff95b6ff900a8d413ebb
Justin Cullen <jcullen@mit.edu>
2012-04-30 14:24:01 -0400

    done

--- xmonad.linkme/xmonad.hs
+++ xmonad.linkme/xmonad.hs
@@ -27,26 +27,28 @@ import XMonad.Prompt
 import XMonad.Prompt.Man
 import XMonad.Prompt.AppendFile
 
+import XMonad.Actions.SpawnOn
+
 -- make sure to edit paths to xmobar and .xmobarrc to match your system.
     -- If xmobar is in your $PATH, and its config is in ~/.xmobarrc you don't
     -- need the xmobar path or config file, use: xmproc <- spawnPipe "xmobar"
 
 -- specify a custom layout hook.
 myLayoutHook =
- 
+
     -- automatically avoid overlapping my dzen status bar.
     avoidStrutsOn [U] $                                        -- (3)
- 
+
     -- make manual gap adjustment possible.
     gaps (zip [U,D,L,R] (repeat 0)) $
- 
+
     -- start all workspaces in my home directory, with the ability
     -- to switch to a new working dir.                          -- (10,11)
     workspaceDir "~" $
- 
+
     -- navigate directionally rather than with mod-j/k
     configurableNavigation (navigateColor "#00aa00") $          -- (8)
- 
+
     -- ability to toggle between fullscreen, reflect x/y, no borders,
     -- and mirrored.
     mkToggle1 NBFULL $                                  -- (14)
@@ -54,28 +56,28 @@ myLayoutHook =
     mkToggle1 REFLECTY $                                -- (14,13)
     mkToggle1 NOBORDERS $                               --  "
     mkToggle1 MIRROR $                                  --  "
- 
+
     -- borders automatically disappear for fullscreen windows.
     smartBorders $                                              -- (7)
- 
+
     -- "web" and "irc" start in Full mode and can switch to tiled...
-    onWorkspaces ["web","chat","media"] (Full ||| myTiled) $               -- (10,0)
- 
+    onWorkspaces ["www","media"] (Full ||| myTiled) $               -- (10,0)
+
     -- ...whereas all other workspaces start tall and can switch
     -- to a grid layout with the focused window magnified.
     myTiled |||           -- resizable tall layout
     Mag.magnifier Grid |||                                      -- (15,6)
-    TwoPane (3/100) (1/2) |||
+    -- TwoPane (3/100) (1/2) |||
     (named "Full|Acc" $ combineTwo myTiled Full Accordion)      -- (15b)
 
 -- use ResizableTall instead of Tall, but still call it "Tall".
 myTiled = named "Tall" $ ResizableTall 1 0.03 0.5 []            -- (9,5)
- 
+
 -- Define terminal
 myTerminal = "urxvt"
 
 -- Define amount and names of workspaces
-myWorkspaces = ["main","dev","www","media","mail","chat"]
+myWorkspaces = ["main","dev","www","media"] ++ (map show [5..10])
 
 -- Define the workspace each application goes to
 myManageHook = composeAll
@@ -105,27 +107,40 @@ main = do
         } `additionalKeysP`
         [ ("M4-n", appendFilePrompt defaultXPConfig "/home/jcullen/notes.txt")
         , ("M4-m", manPrompt defaultXPConfig)
-        , ("M-C-<Space>", sendMessage $ Toggle NBFULL)              -- (14)
-        , ("M-C-x",       sendMessage $ Toggle REFLECTX)            -- (14,13)
-        , ("M-C-y",       sendMessage $ Toggle REFLECTY)            -- (14,13)
-        , ("M-C-m",       sendMessage $ Toggle MIRROR)              --  "
-        , ("M-C-b",       sendMessage $ Toggle NOBORDERS)           --  "
+        , ("M-C-<Space>", sendMessage $ Toggle NBFULL)              
+        , ("M-C-x",       sendMessage $ Toggle REFLECTX)            
+        , ("M-C-y",       sendMessage $ Toggle REFLECTY)            
+        , ("M-C-m",       sendMessage $ Toggle MIRROR)              
+        , ("M-C-b",       sendMessage $ Toggle NOBORDERS)           
+
+        -- keybindings for common programs
+        , ("M-x f", spawnOn "www" "firefox")                             
+        , ("M-x c", spawnOn "media" "clementine")                             
+        , ("M-x p", spawnOn "main" "pcmanfm")                             
 
         -- window navigation keybindings.
-        , ("M4-l", sendMessage $ Go R)                             -- (8)
-        , ("M4-h", sendMessage $ Go L)                             --  "
-        , ("M4-k", sendMessage $ Go U)                             --  "
-        , ("M4-j", sendMessage $ Go D)                             --  "
-        , ("M4-S-l", sendMessage $ Swap R)                             -- (8)
-        , ("M4-S-h", sendMessage $ Swap L)                             --  "
-        , ("M4-S-k", sendMessage $ Swap U)                             --  "
-        , ("M4-S-j", sendMessage $ Swap D)                             --  "
+        , ("M4-l", sendMessage $ Go R)                             
+        , ("M4-h", sendMessage $ Go L)                             
+        , ("M4-k", sendMessage $ Go U)                             
+        , ("M4-j", sendMessage $ Go D)                             
+        , ("M4-S-l", sendMessage $ Swap R)                         
+        , ("M4-S-h", sendMessage $ Swap L)                         
+        , ("M4-S-k", sendMessage $ Swap U)                         
+        , ("M4-S-j", sendMessage $ Swap D)                         
+        , ("M4-S-C-l", sendMessage $ Move R)                         
+        , ("M4-S-C-h", sendMessage $ Move L)                         
+        , ("M4-S-C-k", sendMessage $ Move U)                         
+        , ("M4-S-C-j", sendMessage $ Move D)                         
 
         , ("M4-S-l", spawn "xscreensaver-command -lock")
+
+        -- volume control
         , ("M4-S-v", spawn "urxvt -e alsamixer")
-  
+        , ("<XF86AudioMute>", spawn "amixer -q set Master toggle")
+        , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 5%- unmute")
+        , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 5%+ unmute")
         ]                  
-        
+
 
 startup :: X ()
 startup = do spawn "startup.sh"
