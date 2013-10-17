b84f94f36fb74488212a1bdd98de837e6f061683
Phenguin <jcullen@mit.edu>
2013-10-17 18:14:49 -0400

    Cleanup vimrc and stuff

--- xmonad.linkme/xmonad.hs
+++ xmonad.linkme/xmonad.hs
@@ -1,7 +1,9 @@
 import XMonad
 import XMonad.Config.Xfce
+import XMonad.Hooks.SetWMName
 
 main = xmonad xfceConfig { 
-           modMask = mod4Mask,
-           terminal = "urxvt"
+             modMask = mod4Mask
+           , startupHook = setWMName "LG3D"
+           , terminal = "urxvt"
            }
