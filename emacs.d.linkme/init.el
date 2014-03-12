49ff7aec3d13977c880a6e231c4eb397e5f6a301
Phenguin <jcullen@mit.edu>
2014-03-12 18:07:44 -0400

    Screwing around with emacs config.. just backing up

---
+++ emacs.d.linkme/init.el
@@ -0,0 +1,53 @@
+;;; -*- lexical-binding: t -*-
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                        ("marmalade" . "http://marmalade-repo.org/packages/")
+                        ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+; (global-linum-mode t)
+(package-initialize)
+
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode t)
+
+(ido-mode 1)
+;(smex-initialize)
+(setq x-select-enable-cllipboard t)
+
+; Python-mode customization
+
+; Evil mode customization
+
+(require 'evil)
+(evil-mode 1)
+
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+ 
+(evil-define-command cofi/maybe-exit ()
+  :repeat change
+  (interactive)
+  (let ((modified (buffer-modified-p)))
+    (insert "j")
+    (let ((evt (read-event (format "Insert %c to exit insert state" ?k)
+               nil 0.5)))
+      (cond
+       ((null evt) (message ""))
+       ((and (integerp evt) (char-equal evt ?k))
+    (delete-char -1)
+    (set-buffer-modified-p modified)
+    (push 'escape unread-command-events))
+       (t (setq unread-command-events (append unread-command-events
+                          (list evt))))))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
