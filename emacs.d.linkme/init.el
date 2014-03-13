8b75cbdcac61b146fa077014ef81264a7c7d4a47
Phenguin <jcullen@mit.edu>
2014-03-12 22:12:11 -0400

    Getting towards a better emacs setup

--- emacs.d.linkme/init.el
+++ emacs.d.linkme/init.el
@@ -1,26 +1,52 @@
+;; Load ELPA  
 ;;; -*- lexical-binding: t -*-
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                        ("marmalade" . "http://marmalade-repo.org/packages/")
-                        ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-; (global-linum-mode t)
+(require 'package)
+(setq package-enable-at-startup nil)
 (package-initialize)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'load-path "~/.emacs.d/elpa")
+(require 'cl)
+;; Guarantee all packages are installed on start
+(defvar packages-list
+  '(evil 
+    color-theme-solarized 
+    smex
+    helm
+    auto-complete
+    python-mode
+    flycheck
+    flycheck-haskell)
+  "List of packages needs to be installed at launch")
 
-(require 'auto-complete)
-(require 'auto-complete-config)
-(ac-config-default)
-(global-auto-complete-mode t)
+(defun has-package-not-installed ()
+  (loop for p in packages-list
+        when (not (package-installed-p p)) do (return t)
+        finally (return nil)))
+(when (has-package-not-installed)
+  ;; Check for new packages (package versions)
+  (message "%s" "Get latest versions of all packages...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; Install the missing packages
+  (dolist (p packages-list)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
-(ido-mode 1)
-;(smex-initialize)
-(setq x-select-enable-cllipboard t)
+; General customization
 
-; Python-mode customization
+(load-theme 'solarized-dark)
+(global-linum-mode t)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ; Evil mode customization
-
-(require 'evil)
 (evil-mode 1)
+(helm-mode 1)
 
 (define-key evil-insert-state-map "j" #'cofi/maybe-exit)
  
@@ -39,12 +65,14 @@
     (push 'escape unread-command-events))
        (t (setq unread-command-events (append unread-command-events
                           (list evt))))))))
+
+; From solarized color scheme apparently?
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" default))))
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
