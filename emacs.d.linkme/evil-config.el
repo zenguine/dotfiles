0356276dec342fd9356eccb012bee35a0c22f351
Phenguin <jcullen@mit.edu>
2014-04-10 05:11:31 -0400

    Add evil mode state based cursor highlighting

--- emacs.d.linkme/evil-config.el
+++ emacs.d.linkme/evil-config.el
@@ -1,6 +1,15 @@
 (require 'evil)
 (require 'cl)
 
+(setq evil-magic 'very-magic)
+
+(setq evil-emacs-state-cursor '("red" box))
+(setq evil-normal-state-cursor '("green" box))
+(setq evil-visual-state-cursor '("orange" box))
+(setq evil-insert-state-cursor '("red" bar))
+(setq evil-replace-state-cursor '("red" bar))
+(setq evil-operator-state-cursor '("red" hollow))
+
 (evil-define-command cofi/maybe-exit ()
   :repeat change
   (interactive)
