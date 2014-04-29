c3aaac7904afd20835efc35396018cf1e27fea2a
Phenguin <jcullen@mit.edu>
2014-04-28 22:41:10 -0400

    Fancy symbols in inbox

--- emacs.d.linkme/mu4e-config.el
+++ emacs.d.linkme/mu4e-config.el
@@ -7,6 +7,22 @@
 (setq mu4e-sent-folder   "/sent")
 (setq mu4e-trash-folder  "/archive")
 
+(setq mu4e-headers-seen-mark '("S" . "☑")
+      mu4e-headers-new-mark '("N" .  "✉")
+      mu4e-headers-replied-mark '("R" . "↵")
+      mu4e-headers-passed-mark '("P" . "⇉")
+      mu4e-headers-encrypted-mark '("x" . "⚷")
+      mu4e-headers-signed-mark '("s" . "✍")
+      mu4e-headers-empty-parent-prefix '("-" . "◆")
+      mu4e-headers-first-child-prefix '("\\" . "▶")
+      mu4e-use-fancy-chars t)
+
+                                        ; fix weird html2text is out of range error 'error in process filter: Args out of range: "Email\"", 7, 6'
+; see: https://github.com/djcb/mu/issues/73
+(setq mu4e-html2text-command "html2text -nobs -width 72")
+(setq mu4e-view-prefer-html t)              ;; prefer html
+(setq mu4e-msg2pdf "~/.emacs.d/lib/mu4e/toys/msg2pdf/msg2pdf")
+
 ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
 (setq mu4e-sent-messages-behavior 'delete)
 
@@ -23,6 +39,7 @@
 
 ;; allow for updating mail using 'U' in the main view:
 (setq mu4e-get-mail-command "offlineimap")
+(setq mu4e-update-interval 600)
 
 (setq mu4e-headers-skip-duplicates t)
 (setq mu4e-confirm-quit nil)
@@ -55,8 +72,6 @@
 
 (defun my-mu4e-mode-hook ()
   (setq show-trailing-whitespace nil))
-
-(add-hook mu4e-headers-)
 
 ;;----------------------------------------------------------
 ;; ---- END Email client ----
