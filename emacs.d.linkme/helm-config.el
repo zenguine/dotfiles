859e766561b7080606b04ef32f1736736a0f04a3
Phenguin <jcullen@mit.edu>
2014-05-11 01:49:51 -0400

    Misc changes
    - Backtrace popwin config
    - Helm buffers fuzzy matching

--- emacs.d.linkme/helm-config.el
+++ emacs.d.linkme/helm-config.el
@@ -1,4 +1,6 @@
+(setq helm-buffers-fuzzy-matching t)
 (require 'helm)
+
 
 (defun helm-multi-occur-in-this-mode ()
   "Show all lines matching REGEXP in buffers with this major mode."
--- emacs.d.linkme/popwin-config.el
+++ emacs.d.linkme/popwin-config.el
@@ -4,6 +4,7 @@
 (setq display-buffer-function 'popwin:display-buffer)
 (push '("^\*pytest.+\*$" :regexp t :height 20 :noselect t) popwin:special-display-config)
 (push '("^\*helm.+\*$" :regexp t :height 20) popwin:special-display-config)
+(push '("*Backtrace*" :height 20) popwin:special-display-config)
 
 (defadvice popwin:close-popup-window (before advice-for-before-close-popup-window activate)
   (when popwin:popup-buffer
