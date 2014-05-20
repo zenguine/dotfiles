e5eed7249559abad19af8cddfaea90ece5ef9074
Phenguin <jcullen@mit.edu>
2014-05-20 11:49:19 -0400

    Some eshell stuff

--- emacs.d.linkme/config/eshell-config.el
+++ emacs.d.linkme/config/eshell-config.el
@@ -1,6 +1,15 @@
 (defun eshell/x (&rest args)
-  (interactive)
   (delete-single-window))
+
+
+(defun delete-single-window (&optional window)
+  "Remove WINDOW from the display.  Default is `selected-window'."
+  (interactive)
+  (setq window (or window (selected-window)))
+  (select-window window)
+  (kill-buffer)
+  (if (not (one-window-p t))
+      (delete-window (selected-window))))
 
 (defun eshell/clear (&rest args)
   (interactive)
@@ -19,10 +28,19 @@
   (local-set-key (kbd "C-d") 'eshell-send-eof-to-process)
   (local-unset-key (kbd "C-j"))
   (setq-local show-trailing-whitespace nil)
-  (add-to-list 'eshell-visual-commands "watch"))
+  (add-to-list 'eshell-visual-commands "watch")
+  (add-to-list 'eshell-visual-commands "ssh")
+  (add-to-list 'eshell-visual-commands "tail")
+  (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color)
+  (setenv "PAGER" "cat"))
 
 (eval-after-load 'esh-opt
   '(progn
      (add-hook 'eshell-mode-hook 'my-eshell-mode-hook)))
+
+(add-hook 'emacs-startup-hook #'(lambda ()
+                                  (let ((default-directory (getenv "HOME")))
+                                    (command-execute 'eshell)
+                                    (kill-buffer))))
 
 (provide 'eshell-config)
