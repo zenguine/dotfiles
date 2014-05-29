1dc0de817b24bb7def2a2360430fae475d0533ae
Phenguin <jcullen@mit.edu>
2014-05-29 09:41:45 -0400

    Xmonad changes with switch to ubuntu/gnome

--- scripts.linkme/startup.sh
+++ scripts.linkme/startup.sh
@@ -2,35 +2,11 @@
 
 xrdb -merge .Xresources &
 
-# Setup background images
-eval $(cat ~/.fehbg) &
-
-# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --height 20 --transparent true --tint 0x000000 &
-
-xscreensaver -nosplash &
-
-gnome-settings-daemon &
-
-if [ -x /usr/bin/gnome-power-manager ] ; then
-   sleep 1
-   gnome-power-manager &
-fi
-
-if [ -x /usr/bin/nm-applet ] ; then
-   nm-applet --sm-disable &
-fi
-
 # Start up gpg-agent
-. ~/scripts/gpg-agent.sh
+# . ~/scripts/gpg-agent.sh
 
-dropbox start &
-gtk-redshift -l 42.21:-71.5 -t 5700:3100 &
-# firefox &
-# clementine &
+# dropbox start &
+redshift &
 deluge &
 t-init & # Set up initial tmux environment
-
-# export WINDOW_MANAGER=xmonad
-# gnome-session &
+xmodmap ~/.xmodmaprc &
--- xmonad.linkme/xmonad.hs
+++ xmonad.linkme/xmonad.hs
@@ -1,9 +1,19 @@
 import XMonad
-import XMonad.Config.Xfce
-import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig
+import XMonad.Config.Gnome
 
-main = xmonad xfceConfig { 
-             modMask = mod4Mask
-           , startupHook = setWMName "LG3D"
-           , terminal = "urxvt"
-           }
+main = xmonad $ gnomeConfig { 
+             modMask = mod4Mask,
+             startupHook = do
+                 startupHook gnomeConfig
+                 myStartupHook
+           , terminal = "urxvt" }
+           `additionalKeysP`
+           [ ("M-o", spawn "ALTERNATE_EDITOR=\"\" emacsclient -c")
+           , ("M-f", spawn "firefox")
+           , ("M-p", spawn "synapse") ]
+
+myStartupHook :: X ()
+myStartupHook = do
+    spawn "~/scripts/startup.sh"
+
