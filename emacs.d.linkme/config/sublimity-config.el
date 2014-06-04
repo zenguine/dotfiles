3309035921d957c25b0125e7cceed45cbaa97223
Phenguin <jcullen@mit.edu>
2014-06-04 08:58:46 -0400

    Okay I do like smooth scrolling a little bit..

--- emacs.d.linkme/config/sublimity-config.el
+++ emacs.d.linkme/config/sublimity-config.el
@@ -1,11 +1,11 @@
 (require 'sublimity)
-;; (require 'sublimity-map)
-;; (require 'sublimity-scroll)
+(require 'sublimity-map)
+(require 'sublimity-scroll)
 
-; ;(setq sublimity-map-size 20)
-; ;(setq sublimity-map-fraction 0.3)
-; ;(setq sublimity-map-text-scale -7)
-; ;(sublimity-mode t)
-; ;(sublimity-map-set-delay 6)
+(setq sublimity-map-size 20)
+(setq sublimity-map-fraction 0.3)
+(setq sublimity-map-text-scale -7)
+(sublimity-mode t)
+(sublimity-map-set-delay 6)
 
 (provide 'sublimity-config)
