97dbac11e2ec66cde1d624f5db9ed7455bf17504
Phenguin <jcullen@mit.edu>
2014-07-27 02:02:51 -0400

    Popwin config

--- emacs.d.linkme/config/popwin-config.el
+++ emacs.d.linkme/config/popwin-config.el
@@ -25,6 +25,7 @@
 
 (push '("^\*pytest.+\*$" :regexp t :height 20 :noselect t) popwin:special-display-config)
 (push '("*GHC Info*" :noselect t) popwin:special-display-config)
+(push '("*Warnings*" :noselect t) popwin:special-display-config)
 (push '("^\*ag.+\*$" :regexp t :height 20 :noselect t) popwin:special-display-config)
 (push '("*Backtrace*" :height 20) popwin:special-display-config)
 (push '("*Org-Babel Results*" :height 20) popwin:special-display-config)
