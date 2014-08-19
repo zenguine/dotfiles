55d96e3b0ad134fda24a825dd67ef6a7913131c5
Phenguin <jcullen@mit.edu>
2014-08-19 09:10:50 -0400

    Change up evil state cursor colors

--- emacs.d.linkme/config/evil-config.el
+++ emacs.d.linkme/config/evil-config.el
@@ -7,12 +7,13 @@
 (require 'evil)
 (require 'cl)
 
-(setq evil-emacs-state-cursor '("red" box))
-(setq evil-normal-state-cursor '("green" box))
+(setq evil-emacs-state-cursor '("#fb4934" box))
+(setq evil-normal-state-cursor '("#d3869b" box))
 (setq evil-visual-state-cursor '("orange" box))
-(setq evil-insert-state-cursor '("red" bar))
-(setq evil-replace-state-cursor '("red" bar))
-(setq evil-operator-state-cursor '("red" hollow))
+(setq evil-insert-state-cursor '("maroon" bar))
+(setq evil-replace-state-cursor '("maroon" bar))
+(setq evil-operator-state-cursor '("maroon" hollow))
+
 (setq evil-search-module 'evil-search)
 (setq evil-ex-search-vim-style-regexp t)
 (setq evil-magic 'very-magic)
