d6007accc8d50c16df6e42b4a938f5db1a6011c6
Phenguin <jcullen@mit.edu>
2014-08-27 18:14:53 -0400

    Remove auto-complete and el-get completely

--- emacs.d.linkme/init.el
+++ emacs.d.linkme/init.el
@@ -28,7 +28,6 @@
   '(
     ace-jump-mode
     ag
-    auto-complete
     dash
     dash-functional
     diminish
@@ -106,32 +105,6 @@
     (when (not (package-installed-p p))
       (package-install p))))
 
-;; ---------------------------------------------
-;; El-get initialization
-;; ---------------------------------------------
-
-;; EL-GET
-(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(defun el-get-sync-recipes (overlay)
-  (let* ((recipe-glob (locate-user-emacs-file (concat overlay "/recipes/*.rcp")))
-         (recipe-files (file-expand-wildcards recipe-glob))
-         (recipes (mapcar 'el-get-read-recipe-file recipe-files)))
-    (print recipe-glob)
-    (mapcar (lambda (r) (add-to-list 'el-get-sources r)) recipes)
-    (el-get 'sync (mapcar 'el-get-source-name recipes))))
-(setq el-get-user-package-directory user-emacs-directory)
-
-;; EL-GET SYNC OVERLAYS
-(el-get-sync-recipes "el-get-user")
-
 ;; --------------------------------
 ;; My personal configuration stuff
 ;; --------------------------------
@@ -187,7 +160,8 @@
 (require 'python-config)
 (require 'lisp-config)
 (require 'scheme-config)
-(require 'haskell-config)
 (require 'idris-config)
 
 (require 'keybindings)
+
+(require 'haskell-config)
