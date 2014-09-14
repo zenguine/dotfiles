a9bd89fc4994e672facc8bd37569d2b098b0099b
Phenguin <jcullen@mit.edu>
2014-09-13 22:50:54 -0400

    Add C-c C-l binding to eval buffer/region

--- emacs.d.linkme/config/lang/lisp-config.el
+++ emacs.d.linkme/config/lang/lisp-config.el
@@ -1,5 +1,17 @@
 (define-key emacs-lisp-mode-map (kbd "C-c C-e") 'pp-macroexpand-last-sexp)
 
+(defun prompt-to-eval ()
+  "Yes or no prompt user and if yes, evaluate the current buffer as
+  elisp"
+  (interactive)
+  (let* ((info (if (region-active-p)
+		   '("Evaluate the current region as elisp?: " . eval-region)
+		 '("Evaluate the buffer as elisp?: " . eval-buffer)))
+	 (prompt-string (car info))
+	 (eval-func (cdr info)))
+    (if (y-or-n-p prompt-string)
+	(call-interactively eval-func))))
+
 (defun my-elisp-mode-hook ()
   (modify-syntax-entry ?- "w")
   (pretty-mode t)
@@ -21,6 +33,7 @@
 (defun my-lisp-interaction-mode-hook ()
   (local-unset-key (kbd "C-j"))
   (define-key emacs-lisp-mode-map (kbd "C-0") 'eval-defun)
+  (define-key emacs-lisp-mode-map (kbd "C-c C-l") 'prompt-to-eval)
   (pretty-mode t)
   (rainbow-delimiters-mode t)
   (elisp-slime-nav-mode t))
