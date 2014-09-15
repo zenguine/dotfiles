3d073b604b11514d11be931e8e3ffe254d1605f4
Phenguin <jcullen@mit.edu>
2014-09-15 09:30:33 -0400

    Change back to the best font.. envy

--- emacs.d.linkme/custom.el
+++ emacs.d.linkme/custom.el
@@ -59,5 +59,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil :family "Fantasque Sans Mono" :foundry "unknown" :slant normal :weight normal :height 90 :width normal))))
+ '(default ((t (:background nil :family "Envy Code R" :foundry "unknown" :slant normal :weight normal :height 98 :width normal))))
  '(shm-current-face ((t (:background "#295864")))))
