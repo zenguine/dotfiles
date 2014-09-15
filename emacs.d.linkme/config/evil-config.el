632fdbc0a094878afaf565dcefbcf12a9dbf8f95
Phenguin <jcullen@mit.edu>
2014-09-15 03:18:55 -0400

    Add some more bindings to edit common files

--- emacs.d.linkme/config/evil-config.el
+++ emacs.d.linkme/config/evil-config.el
@@ -120,9 +120,9 @@
 ;; keybindings to edit specific files
 (def-key evil-normal-state-map " eo" 'find-file (f-join org-files-home "personal.org"))
 (define-key evil-normal-state-map " em" 'edit-mode-config-file)
-(define-key evil-normal-state-map " em" 'edit-mode-config-file)
-(def-key evil-normal-state-map " ev" 'find-file "~/.vimrc")
+(def-key evil-normal-state-map " ek" 'find-file (f-join user-emacs-directory "config" "keybindings.el"))
 (def-key evil-normal-state-map " ee" 'find-file user-init-file)
+(def-key evil-normal-state-map " ev" 'find-file (f-join user-emacs-directory "config" "evil-config.el"))
 
 
 (define-key evil-normal-state-map " l" 'linum-mode)
