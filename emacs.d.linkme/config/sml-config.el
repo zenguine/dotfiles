8533890cd00d8133fb92021d5cef99388271f80c
Phenguin <jcullen@mit.edu>
2014-09-15 09:22:43 -0400

    FInish configuration sml

--- emacs.d.linkme/config/sml-config.el
+++ emacs.d.linkme/config/sml-config.el
@@ -4,9 +4,10 @@
 (setq sml/shorten-modes t)
 (setq sml/name-width 40)
 (setq sml/mode-width 'full)
+(setq sml/no-confirm-load-theme)
+(setq sml/theme 'respectful)
 
 (sml/setup)
-
 
 (add-to-list 'sml/replacer-regexp-list '("^~/.dotfiles/emacs.d.linkme/" ":ED:"))
 
