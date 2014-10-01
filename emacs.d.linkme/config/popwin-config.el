324fc9968e2d0789b9eb83a7303f194ab6372afe
Phenguin <jcullen@mit.edu>
2014-10-01 03:57:33 -0400

    Mess with popwin config

--- emacs.d.linkme/config/popwin-config.el
+++ emacs.d.linkme/config/popwin-config.el
@@ -4,6 +4,9 @@
 (require 'popwin)
 
 (setq display-buffer-function 'popwin:display-buffer)
+(setq popwin:popup-window-position 'right)
+(setq popwin:popup-window-height 0.3)
+(setq popwin:popup-window-width 0.4)
 
 (defun make-buffer-name-matches-regexp-p (re)
   (lambda (buf)
@@ -23,15 +26,15 @@
 
 (popwin-mode 1)
 
-(push '("^\*pytest.+\*$" :regexp t :height 30 :noselect t) popwin:special-display-config)
+(push '("^\\*pytest.+\\*$" :regexp t ::noselect t) popwin:special-display-config)
 (push '("*GHC Info*" :noselect t) popwin:special-display-config)
-(push '("*company-documentation*" :noselect t) popwin:special-display-config)
+(push '("*company-documentation*" :position bottom :height 15 :noselect t) popwin:special-display-config)
 (push '("*Warnings*" :noselect t) popwin:special-display-config)
-(push '("^\*ag.+\*$" :regexp t :height 30 :noselect t) popwin:special-display-config)
-(push '("*Backtrace*" :height 30) popwin:special-display-config)
-(push '("*Org-Babel Results*" :height 30) popwin:special-display-config)
-(push '("*jedi:doc*" :height 30) popwin:special-display-config)
-(push '("*Help*" :height 15 :stick t) popwin:special-display-config)
+(push '("^\\*ag.+\\*$" :regexp t :position bottom :height 0.4 :noselect t) popwin:special-display-config)
+(push '("*Backtrace*") popwin:special-display-config)
+(push '("*Org-Babel Results*" :position bottom :height 0.2) popwin:special-display-config)
+(push '("*jedi:doc*") popwin:special-display-config)
+(push '("*Help*" :position bottom :height 0.5 :stick t :noselect t) popwin:special-display-config)
 (push '("*Org Agenda*" :width 0.5 :position right :stick t) popwin:special-display-config)
 
 (provide 'popwin-config)
