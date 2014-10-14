fbfbf336c39fadf0f9e34549f7feae38eb55d5e0
Phenguin <jcullen@mit.edu>
2014-10-13 23:46:44 -0400

    Remove border on xmonad fullscreen

--- xmonad.linkme/xmonad.hs
+++ xmonad.linkme/xmonad.hs
@@ -1,23 +1,23 @@
 import           XMonad
 import           XMonad.Config.Gnome
+import           XMonad.Layout.NoBorders
 import           XMonad.Util.EZConfig
 
+main :: IO ()
 main = xmonad $ gnomeConfig {
-             modMask = mod4Mask,
-             startupHook = do
-                 startupHook gnomeConfig
-                 myStartupHook
-           , terminal = "urxvt" }
-           `additionalKeysP`
-           [ ("M-o", spawn "echo 'ALTERNATE_EDITOR=\"\" emacsclient -c & exit' | zsh")
-           , ("M-f", spawn "firefox")
-           , ("M-m", spawn "banshee")
-           , ("M-F1", spawn "synclient TouchPadOff=1")
-           , ("M-F2", spawn "synclient TouchPadOff=0")
-           , ("M-m", spawn "banshee")
-           , ("M-p", spawn "synapse") ]
+  modMask = mod4Mask,
+  layoutHook = smartBorders . layoutHook $ gnomeConfig,
+  startupHook = do startupHook gnomeConfig
+                   myStartupHook
+  , terminal = "urxvt" }
+       `additionalKeysP`
+       [ ("M-o", spawn "echo 'ALTERNATE_EDITOR=\"\" emacsclient -c & exit' | zsh")
+       , ("M-f", spawn "firefox")
+       , ("M-m", spawn "banshee")
+       , ("M-F1", spawn "synclient TouchPadOff=1")
+       , ("M-F2", spawn "synclient TouchPadOff=0")
+       , ("M-m", spawn "banshee")
+       , ("M-p", spawn "echo 'ALTERNATE_EDITOR=\"\" emacsclient -c & exit' | zsh") ]
 
 myStartupHook :: X ()
-myStartupHook = do
-    spawn "~/scripts/startup.sh"
-
+myStartupHook = spawn "~/scripts/startup.sh"
