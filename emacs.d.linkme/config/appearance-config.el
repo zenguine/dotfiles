30ed744aa0fef0b892caa0923e373e3051e31886
Phenguin <jcullen@mit.edu>
2014-10-22 05:13:50 -0400

    Change to zenburn

--- emacs.d.linkme/config/appearance-config.el
+++ emacs.d.linkme/config/appearance-config.el
@@ -84,7 +84,7 @@ on a per-theme basis."
     (load-theme chosen t)
     chosen))
 
-(load-theme 'afternoon t)
+(load-theme 'zenburn t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Diminish -- stop minor modes from cluttering up my modeline
