d3906d04fdd52210d1c9a5c8108ec35534b6b5e0
Phenguin <jcullen@mit.edu>
2014-11-18 14:05:33 -0500

    Theme flatland

--- emacs.d.linkme/config/appearance-config.el
+++ emacs.d.linkme/config/appearance-config.el
@@ -84,7 +84,7 @@ on a per-theme basis."
     (load-theme chosen t)
     chosen))
 
-(load-theme 'gotham t)
+(load-theme 'flatland t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Diminish -- stop minor modes from cluttering up my modeline
