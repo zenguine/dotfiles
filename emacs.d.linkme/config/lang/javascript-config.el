fb1c2ac450e1b7bc24972a50ca23f12dbabf9be0
Phenguin <jcullen@mit.edu>
2014-12-02 03:12:36 -0500

    Use js2-mode and skewer-mode

--- emacs.d.linkme/config/lang/javascript-config.el
+++ emacs.d.linkme/config/lang/javascript-config.el
@@ -1,5 +1,13 @@
+(require 'js2-mode)
+(require 'skewer-mode)
+
 (defun my-javascript-hook ()
-  (increase-company-delay-locally 2)
-  )
+  (increase-company-delay-locally 2))
+
+;; Use js2-mode instead of default js-mode
+(add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js2-minor-mode-hook 'skewer-mode)
+(add-hook 'js2-mode-hook 'skewer-mode)
 
 (provide 'javascript-config)
