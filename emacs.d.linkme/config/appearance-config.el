4c9ceed8e43f2d27544fb45ffc0509df5b063acd
Phenguin <jcullen@mit.edu>
2015-01-02 10:17:07 -0500

    Why does zenburn always win

--- emacs.d.linkme/config/appearance-config.el
+++ emacs.d.linkme/config/appearance-config.el
@@ -95,7 +95,7 @@ on a per-theme basis."
     (load-theme chosen t)
     chosen))
 
-(load-theme 'flatland t)
+(load-theme 'zenburn t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Diminish -- stop minor modes from cluttering up my modeline
