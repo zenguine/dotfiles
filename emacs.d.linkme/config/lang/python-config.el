e5ff47647ffc88ddb2cdcd03f7703547b0f1442b
Phenguin <jcullen@mit.edu>
2015-12-03 18:23:10 -0500

    Fix python configuration..

--- emacs.d.linkme/config/lang/python-config.el
+++ emacs.d.linkme/config/lang/python-config.el
@@ -1,6 +1,5 @@
-(use-package elpy
-  :ensure t
-  :commands python-mode
+(use-package python
+  :commands (python-mode)
   :config
   (setq
    python-shell-interpreter "ipython"
@@ -27,40 +26,49 @@
     "K" 'jedi:show-doc
     (kbd "C-]") 'jedi:goto-definition
     (kbd "C-t") 'jedi:goto-definition-pop-marker)
+  (defun my-python-mode-hook ()
+    (local-set-key (kbd "C-c !") 'python-shell-switch-to-shell)
+    (local-set-key (kbd "C-c T a") 'pytest-test-all)
+    (local-set-key (kbd "C-c T f") 'pytest-test-current-file)
+    (local-set-key (kbd "C-c T t") 'pytest-test-specific-test)
+    (local-set-key (kbd "C-c C-l") 'my-ipython-shell-load-file-as-module)
+    (hs-minor-mode t)
+    (modify-syntax-entry ?_ "w")
+    ;; (flymake-mode -1)
+    (auto-complete-mode -1)
+    (elpy-mode))
 
-  (defun my-ipython-shell-get-or-create-process ()
-    "Get or create an inferior Python process for current buffer and return it."
-    (let* ((dedicated-proc-name (python-shell-get-process-name t))
-	   (dedicated-proc-buffer-name (format "*%s*" dedicated-proc-name))
-	   (global-proc-name  (python-shell-get-process-name nil))
-	   (global-proc-buffer-name (format "*%s*" global-proc-name))
-	   (dedicated-running (comint-check-proc dedicated-proc-buffer-name))
-	   (global-running (comint-check-proc global-proc-buffer-name))
-	   (current-prefix-arg 16))
-      (when (and (not dedicated-running) (not global-running))
-	(if (run-python (executable-find "ipython") t t)
-	    (setq dedicated-running t)
-	  (setq global-running t)))
-      ;; Always prefer dedicated
-      (get-buffer-process (if dedicated-running
-			      dedicated-proc-buffer-name
-			    global-proc-buffer-name))))
+  (add-hook 'python-mode-hook 'my-python-mode-hook)
+
+  (defun my-jedi-setup ()
+    (defmacro add-args (arg-list arg-name arg-value)
+      `(setq ,arg-list (append ,arg-list (list ,arg-name ,arg-value))))
+    (make-local-variable 'jedi:server-args)
+    (let ((proj-package-root (python-package-root t)))
+      (if proj-package-root
+	  (add-args jedi:server-args "--sys-path" proj-package-root)))
+    (jedi:setup)
+    (jedi:create-nested-imenu-index))
+
+  (add-hook 'python-mode-hook 'my-jedi-setup)
 
   (defun python-package-dir-p (dir)
     (file-exists-p (concat dir "__init__.py")))
 
   (defun python-package-root (&optional silent)
     (interactive)
-    (let ((buffer-file-dir (file-name-directory
-			    (expand-file-name
-			     (buffer-file-name (current-buffer))))))
-      (if (python-package-dir-p buffer-file-dir)
-	  (expand-file-name (vc-find-root
-			     buffer-file-dir
-			     (lambda (dir) (not (python-package-dir-p dir)))))
-	(if silent
-	    nil
-	  (error "Current directory is not a python package")))))
+    (let ((buf-fn (buffer-file-name (current-buffer))))
+      (if buf-fn
+          (let ((buffer-file-dir (file-name-directory
+                                  (expand-file-name buf-fn))))
+            (if (python-package-dir-p buffer-file-dir)
+                (expand-file-name (vc-find-root
+                                   buffer-file-dir
+                                   (lambda (dir) (not (python-package-dir-p dir)))))
+              (if silent
+                  nil
+                (error "Current directory is not a python package"))))
+        nil)))
 
   (defun file-python-package-name ()
     (interactive)
@@ -109,49 +117,51 @@
     (if show
 	(my-ipython-shell-switch-to-shell)))
 
-  (defun my-python-mode-hook ()
-    (local-set-key (kbd "C-c !") 'python-shell-switch-to-shell)
-    (local-set-key (kbd "C-c T a") 'pytest-test-all)
-    (local-set-key (kbd "C-c T f") 'pytest-test-current-file)
-    (local-set-key (kbd "C-c T t") 'pytest-test-specific-test)
-    (local-set-key (kbd "C-c C-l") 'my-ipython-shell-load-file-as-module)
-    (hs-minor-mode t)
-    (modify-syntax-entry ?_ "w")
-    ;; (flymake-mode -1)
-    (auto-complete-mode -1))
+  (defun my-ipython-shell-get-or-create-process ()
+    "Get or create an inferior Python process for current buffer and return it."
+    (let* ((dedicated-proc-name (python-shell-get-process-name t))
+	   (dedicated-proc-buffer-name (format "*%s*" dedicated-proc-name))
+	   (global-proc-name  (python-shell-get-process-name nil))
+	   (global-proc-buffer-name (format "*%s*" global-proc-name))
+	   (dedicated-running (comint-check-proc dedicated-proc-buffer-name))
+	   (global-running (comint-check-proc global-proc-buffer-name))
+	   (current-prefix-arg 16))
+      (when (and (not dedicated-running) (not global-running))
+	(if (run-python (executable-find "ipython") t t)
+	    (setq dedicated-running t)
+	  (setq global-running t)))
+      ;; Always prefer dedicated
+      (get-buffer-process (if dedicated-running
+			      dedicated-proc-buffer-name
+			    global-proc-buffer-name))))
 
-  (defun my-jedi-setup ()
-    (defmacro add-args (arg-list arg-name arg-value)
-      `(setq ,arg-list (append ,arg-list (list ,arg-name ,arg-value))))
-    (make-local-variable 'jedi:server-args)
-    (let ((proj-package-root (python-package-root t)))
-      (if proj-package-root
-	  (add-args jedi:server-args "--sys-path" proj-package-root)))
-    (jedi:setup)
-    (jedi:create-nested-imenu-index))
+  (define-key python-mode-map (kbd "C-c C-d") 'helm-pydoc)
+  
+   ;; Ipython notebok setup
+  (after 'ein
+    (global-set-key (kbd "C-x i") 'ein:notebooklist-open))
+  
+  )
 
-  (add-hook 'python-mode-hook 'my-python-mode-hook)
-  (add-hook 'python-mode-hook 'my-jedi-setup)
+(use-package elpy
+  :ensure t
+  :commands (elpy-mode)
+  :init
+  :config
   (setq elpy-rpc-backend "jedi")
-
   (elpy-enable)
   ;; (setq elpy-modules (delete 'elpy-module-flymake elpy-modules))
   (elpy-use-ipython)
 
-  (define-key elpy-mode-map (kbd "C-c C-z") 'my-ipython-shell-switch-to-shell)
+  ;; (define-key elpy-mode-map (kbd "C-c C-z") 'my-ipython-shell-switch-to-shell)
   (define-key elpy-mode-map (kbd "C-c C-r") 'my-ipython-shell-reload-file-module)
-  (def-key elpy-mode-map (kbd "C-c C-c") 'my-python-send-region-or-buffer t)
-  (define-key python-mode-map (kbd "C-c C-d") 'helm-pydoc)
+  ;; (def-key elpy-mode-map (kbd "C-c C-c") 'my-python-send-region-or-buffer t)
   (define-key elpy-mode-map (kbd "C-c C-d") 'my-pydoc-fn)
 
   (defun my-pydoc-fn (arg)
     (interactive "P")
     (if arg
 	(call-interactively 'elpy-pydoc)
-      (call-interactively 'helm-pydoc)))
-
-  ;; Ipython notebok setup
-  (after 'ein
-    (global-set-key (kbd "C-x i") 'ein:notebooklist-open)))
+      (call-interactively 'helm-pydoc))))
 
 (provide 'python-config)
