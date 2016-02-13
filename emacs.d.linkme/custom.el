3ddd1f36c502311689ddf03e7142b19fbc7955ec
Phenguin <jcullen@mit.edu>
2016-02-13 02:12:58 -0500

    CHange emacs font to terminus

--- emacs.d.linkme/custom.el
+++ emacs.d.linkme/custom.el
@@ -116,7 +116,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil :family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal))))
+ '(default ((t (:background nil :family "Terminus" :foundry "xos4" :slant normal :weight normal :height 120 :width normal))))
  '(company-scrollbar-bg ((t (:background "#414141"))))
  '(company-scrollbar-fg ((t (:background "#343434"))))
  '(company-tooltip ((t (:inherit default :background "#2d2d2d"))))
